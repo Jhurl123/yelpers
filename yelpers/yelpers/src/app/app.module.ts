55840ef7b1879e28023abe917d637ec67f544abb
Justin Hurley <justin.hurley@oyova.com>
2019-08-16 00:10:02 -0400

    Initial Commit

---
+++ yelpers/yelpers/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopHeaderComponent } from '@/top-header/top-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopHeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
