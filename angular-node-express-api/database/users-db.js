a5960e109841f35f5b8299d7374d04c05da785c5
Justin Hurley <justin.hurley@oyova.com>
2020-01-12 17:56:09 -0500

    Test recompile

--- angular-node-express-api/database/users-db.js
+++ angular-node-express-api/database/users-db.js
@@ -14,6 +14,7 @@ const pool = new Pool({
   port: 5432,
 });
 
+// Test
 // the pool will emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
 pool.on('error', (err, client) => {
