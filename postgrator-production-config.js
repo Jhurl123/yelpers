7ec40caacef61150de746f4ffc58c72ac659704c
Justin Hurley <justin.hurley@oyova.com>
2020-01-20 20:20:09 -0500

    Addition of DB URL string

--- postgrator-production-config.js
+++ postgrator-production-config.js
@@ -8,5 +8,5 @@ module.exports = {
   "database": process.env.PROD_DB_NAME,
   "username": process.env.PROD_DB_USER,
   "password": process.env.PROD_DB_PASS,
-  "connectionString": process.env.PROD_DB_URL
+  "connectionString": process.env.DATABASE_URL
 }
